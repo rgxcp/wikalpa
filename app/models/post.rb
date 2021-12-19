ed4842c0d4c098afdbac1bf48a723413e036a4fb
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 05:30:23 +0700

    feat: validate length of 'Post' body is at most 255
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,6 @@ class Post < ApplicationRecord
 
   validates :community, presence: true
   validates :user, presence: true
+  validates :body, length: { maximum: 255 }
   validates :body, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe Post, type: :model do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:community) }
   it { is_expected.to validate_presence_of(:user) }
+  it { is_expected.to validate_length_of(:body).is_at_most(255) }
   it { is_expected.to validate_presence_of(:body) }
 end
