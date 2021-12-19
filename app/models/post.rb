f5e75f1d1000412f16bbb48ed1bdb3b512cbd3b7
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 05:26:04 +0700

    feat: add belongs to 'User' association at 'Post'
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :community
+  belongs_to :user
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   it { is_expected.to belong_to(:community) }
+  it { is_expected.to belong_to(:user) }
 end
