10a51e25f161d5a9c012cb7f04dd499f7d17e58f
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 04:27:26 +0700

    feat: validate uniqueness of 'Community' name
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,4 +1,5 @@
 class Community < ApplicationRecord
   validates :name, length: { in: 5..20 }
   validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Community, type: :model do
+  subject(:community) { build(:community) }
+
   it { is_expected.to validate_length_of(:name).is_at_least(5).is_at_most(20) }
   it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
 end
