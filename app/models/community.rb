9a017fa4ab3c5754b6237c528cbbb837134aaaaa
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 04:28:59 +0700

    feat: validate presence of 'Community' description
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/community.rb
+++ app/models/community.rb
@@ -2,4 +2,5 @@ class Community < ApplicationRecord
   validates :name, length: { in: 5..20 }
   validates :name, presence: true
   validates :name, uniqueness: true
+  validates :description, presence: true
 end
--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -6,4 +6,5 @@ RSpec.describe Community, type: :model do
   it { is_expected.to validate_length_of(:name).is_at_least(5).is_at_most(20) }
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name) }
+  it { is_expected.to validate_presence_of(:description) }
 end
