adfcfb37253c2dfc24495c0dd2abec536dae8162
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 06:20:00 +0700

    feat: validate length of 'Reply' body is at most 255
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -4,5 +4,6 @@ class Reply < ApplicationRecord
 
   validates :comment, presence: true
   validates :user, presence: true
+  validates :body, length: { maximum: 255 }
   validates :body, presence: true
 end
--- spec/models/reply_spec.rb
+++ spec/models/reply_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe Reply, type: :model do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:comment) }
   it { is_expected.to validate_presence_of(:user) }
+  it { is_expected.to validate_length_of(:body).is_at_most(255) }
   it { is_expected.to validate_presence_of(:body) }
 end
