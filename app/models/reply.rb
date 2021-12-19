0e8153973d44cc21d2f91add4ffc207e1a5e7492
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 06:18:46 +0700

    feat: validate presence of 'Reply' comment
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,6 @@
 class Reply < ApplicationRecord
   belongs_to :comment
   belongs_to :user
+
+  validates :comment, presence: true
 end
--- spec/models/reply_spec.rb
+++ spec/models/reply_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Reply, type: :model do
   it { is_expected.to belong_to(:comment) }
   it { is_expected.to belong_to(:user) }
+  it { is_expected.to validate_presence_of(:comment) }
 end
