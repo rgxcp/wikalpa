95a96d2ecb5c4ef9ecf7dd2cfa2bdb75c7c7869a
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 04:30:52 +0700

    feat: validate length of 'Community' description is at most 255
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/community.rb
+++ app/models/community.rb
@@ -2,5 +2,6 @@ class Community < ApplicationRecord
   validates :name, length: { in: 5..20 }
   validates :name, presence: true
   validates :name, uniqueness: true
+  validates :description, length: { maximum: 255 }
   validates :description, presence: true
 end
