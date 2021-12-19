bf29f2042a8672c9e188c1936aafa52d91a4f5a4
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 06:08:17 +0700

    feat: validate presence of 'Comment' post
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validates :post, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   it { is_expected.to belong_to(:post) }
   it { is_expected.to belong_to(:user) }
+  it { is_expected.to validate_presence_of(:post) }
 end
