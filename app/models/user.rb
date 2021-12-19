aae7e9cb6da5b183371a2cf690b673a544e46a1c
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 03:57:19 +0700

    feat: validate length of 'User' password is at least 8
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :username, length: { in: 5..15 }
   validates :username, presence: true
   validates :username, uniqueness: true
+  validates :password, length: { minimum: 8 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,5 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_length_of(:username).is_at_least(5).is_at_most(15) }
   it { is_expected.to validate_presence_of(:username) }
   it { is_expected.to validate_uniqueness_of(:username) }
+  it { is_expected.to validate_length_of(:password).is_at_least(8) }
 end
