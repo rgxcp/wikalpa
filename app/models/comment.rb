69f5f3a182e721a4cfed08779a71b8d7b6575434
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 06:11:57 +0700

    feat: remove 'Comment' body trailing spaces before validation
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,8 @@ class Comment < ApplicationRecord
   validates :user, presence: true
   validates :body, length: { maximum: 255 }
   validates :body, presence: true
+
+  before_validation do
+    self.body = body.strip if body.present?
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe Comment, type: :model do
   it { is_expected.to validate_presence_of(:user) }
   it { is_expected.to validate_length_of(:body).is_at_most(255) }
   it { is_expected.to validate_presence_of(:body) }
+
+  describe "before validation" do
+    context "when body contain trailing spaces" do
+      it "is removed before validation" do
+        comment = build(:comment, :unformatted)
+        comment.validate
+        expect(comment.body).to eq("just setting up my wklp")
+      end
+    end
+  end
 end
