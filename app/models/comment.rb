ac37e2c7722c56b0c5575d4e2c9a238f6b74bd50
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 06:10:24 +0700

    feat: validate length of 'Comment' body is at most 255
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,5 +4,6 @@ class Comment < ApplicationRecord
 
   validates :post, presence: true
   validates :user, presence: true
+  validates :body, length: { maximum: 255 }
   validates :body, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe Comment, type: :model do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:post) }
   it { is_expected.to validate_presence_of(:user) }
+  it { is_expected.to validate_length_of(:body).is_at_most(255) }
   it { is_expected.to validate_presence_of(:body) }
 end
