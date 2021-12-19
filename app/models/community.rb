aca5c156f35000244eed19ac3a116f170378c2ee
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 04:25:43 +0700

    feat: validate length of 'Community' name is at least 5
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,3 +1,4 @@
 class Community < ApplicationRecord
+  validates :name, length: { minimum: 5 }
   validates :name, presence: true
 end
--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Community, type: :model do
+  it { is_expected.to validate_length_of(:name).is_at_least(5) }
   it { is_expected.to validate_presence_of(:name) }
 end
