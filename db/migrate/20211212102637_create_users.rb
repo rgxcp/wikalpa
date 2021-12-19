9739a76b2d23e32c15ee36bc72566abd91663adc
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-19 20:48:34 +0700

    chore: add users migration
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

---
+++ db/migrate/20211212102637_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username, limit: 20, null: false, index: { unique: true }
+      t.string :password_digest, limit: 60, null: false
+      t.timestamps
+    end
+  end
+end
