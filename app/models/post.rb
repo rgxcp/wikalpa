f70a2e092f7d84b9abdf23d3d1e83a5ff0856c61
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 05:27:12 +0700

    feat: validate presence of 'Post' community
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :community
   belongs_to :user
+
+  validates :community, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   it { is_expected.to belong_to(:community) }
   it { is_expected.to belong_to(:user) }
+  it { is_expected.to validate_presence_of(:community) }
 end
