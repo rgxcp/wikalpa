6584e845aeccb974a402280c3199382bb2bd1b78
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 03:45:01 +0700

    feat: validate length of 'User' username is at least 5
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :username, length: { minimum: 5 }
   validates :username, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to have_secure_password }
+  it { is_expected.to validate_length_of(:username).is_at_least(5) }
   it { is_expected.to validate_presence_of(:username) }
 end
