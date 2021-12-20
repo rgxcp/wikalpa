308e0b63d8202222b97e1463346d03121f6c3a90
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 17:00:43 +0700

    feat: validate uniqueness of 'Like' user scoped to likeable_type & likeable_id
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,5 @@ class Like < ApplicationRecord
   belongs_to :likeable, polymorphic: true
 
   validates :user, presence: true
+  validates :user, uniqueness: { scope: [:likeable_type, :likeable_id] }
 end
