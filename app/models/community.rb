6e95a7359000ac206f6181a8b3a16326419bafc2
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 19:56:47 +0700

    feat: validate 'Community' name can't contain spaces & non word chars
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,4 +1,5 @@
 class Community < ApplicationRecord
+  validates :name, format: { with: /\A[a-zA-Z0-9.\w]+\z/ }
   validates :name, length: { in: 5..20 }
   validates :name, presence: true
   validates :name, uniqueness: true
