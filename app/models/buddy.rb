ef11a752a253d6c9700cc112aaebb362eaaf4558
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 19:22:03 +0700

    feat: add belongs to 'Buddy' association on 'Buddy'
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/buddy.rb
+++ app/models/buddy.rb
@@ -1,3 +1,4 @@
 class Buddy < ApplicationRecord
   belongs_to :user
+  belongs_to :buddy, class_name: "User"
 end
--- spec/models/buddy_spec.rb
+++ spec/models/buddy_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Buddy, type: :model do
   it { is_expected.to belong_to(:user) }
+  it { is_expected.to belong_to(:buddy).class_name("User") }
 end
