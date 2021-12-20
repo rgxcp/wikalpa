ced2a723dd60fbc2acf73a3a6d7dc9b9404e9e2c
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-20 17:07:18 +0700

    perf: remove unused likeable polymorphic association on 'Like'
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,5 @@
 class Like < ApplicationRecord
   belongs_to :user
-  belongs_to :likeable, polymorphic: true
 
   validates :user, presence: true
   validates :user, uniqueness: { scope: [:likeable_type, :likeable_id] }
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Like, type: :model do
   subject(:like) { build(:like) }
 
   it { is_expected.to belong_to(:user) }
-  it { is_expected.to belong_to(:likeable) }
   it { is_expected.to validate_presence_of(:user) }
   it { is_expected.to validate_uniqueness_of(:user).scoped_to([:likeable_type, :likeable_id]) }
 end
