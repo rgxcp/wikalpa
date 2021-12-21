68c2fee63c54d8178399b8d4b95f4c2081bfba04
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-22 04:44:31 +0700

    feat: validate adding self as buddy
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/buddy.rb
+++ app/models/buddy.rb
@@ -2,7 +2,14 @@ class Buddy < ApplicationRecord
   belongs_to :user
   belongs_to :buddy, class_name: "User"
 
+  validate :adding_self_as_buddy
   validates :user, presence: true
   validates :buddy, presence: true
   validates :buddy, uniqueness: { scope: :user_id }
+
+  private
+
+  def adding_self_as_buddy
+    errors.add(:buddy) if buddy == user
+  end
 end
--- spec/models/buddy_spec.rb
+++ spec/models/buddy_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Buddy, type: :model do
   it { is_expected.to validate_presence_of(:user) }
   it { is_expected.to validate_presence_of(:buddy) }
   it { is_expected.to validate_uniqueness_of(:buddy).scoped_to(:user_id) }
+
+  describe "custom validation" do
+    context "when adding self as buddy" do
+      it "is not valid" do
+        user = create(:user)
+        buddy = build(:buddy, user: user, buddy: user)
+        buddy.validate
+        expect(buddy).not_to be_valid
+      end
+    end
+  end
 end
