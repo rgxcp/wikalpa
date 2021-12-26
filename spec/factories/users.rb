70173655012015e95a98075c22b64cd5676557fd
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-26 16:32:42 +0700

    chore: add invalid user factory trait
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,11 @@ FactoryBot.define do
     sequence(:username) { |n| "john.doe#{n}" }
     password { "12345678" }
 
+    trait :invalid do
+      username { "" }
+      password { "" }
+    end
+
     trait :unformatted do
       username { " JOHN.DOE " }
     end
