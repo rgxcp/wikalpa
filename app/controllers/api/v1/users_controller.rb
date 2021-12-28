6369288d4ae7692d89fcf04b37ea379a6c4faa41
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-28 09:23:30 +0700

    refactor: use forbidden response method
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,10 +2,6 @@ class Api::V1::UsersController < ApplicationController
   before_action :authenticate_request!, only: :update
 
   def update
-    render json: {
-      success: false,
-      message: "Forbidden"
-    },
-    status: :forbidden unless @auth_id == params[:id].to_i
+    forbidden_response unless @auth_id == params[:id].to_i
   end
 end
