614720c46c888a930bab9018522abef52b259a5e
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-28 13:13:09 +0700

    refactor: use forbidden response method
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -19,11 +19,7 @@ class Api::V1::CommunitiesController < ApplicationController
     community = Community.find(params[:id])
     member = Member.exists?(community: community, user: @auth_user)
 
-    render json: {
-      success: false,
-      message: "Forbidden"
-    },
-    status: :forbidden unless member
+    forbidden_response unless member
   end
 
   private
