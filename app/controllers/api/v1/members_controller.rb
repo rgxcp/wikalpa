fe4bcde266761b5ff9f5568750c44b236d6a5706
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-28 17:00:59 +0700

    feat: POST /communities/:id/members returns 404 status code when community not exists
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::MembersController < ApplicationController
   before_action :authenticate_request!, only: :create
 
   def create
+    community = Community.find(params[:community_id])
   end
 end
--- spec/requests/api/v1/members_controller_spec.rb
+++ spec/requests/api/v1/members_controller_spec.rb
@@ -8,5 +8,14 @@ RSpec.describe Api::V1::MembersController, type: :request do
         expect(response).to have_http_status(:unauthorized)
       end
     end
+
+    context "when community not exists" do
+      it "returns 404 status code" do
+        user = create(:user)
+        token = JsonWebToken.encode({ id: user.id })
+        post api_v1_community_members_path(0), headers: { Authorization: "Bearer #{token}" }
+        expect(response).to have_http_status(:not_found)
+      end
+    end
   end
 end
