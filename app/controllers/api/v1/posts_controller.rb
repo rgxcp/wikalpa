f027da8016698ae63ab9385c02c486def715583f
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-29 13:05:04 +0700

    feat: POST /communities/:id/posts returns 404 status code when community not exists
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::PostsController < ApplicationController
   before_action :authenticate_request, only: :create
 
   def create
+    community = Community.find(params[:community_id])
   end
 end
