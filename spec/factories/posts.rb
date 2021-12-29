39fd6ce956fc8f935416de6eb9114ce341fa8b22
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-29 13:19:00 +0700

    chore: add invalid post factory trait
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -4,6 +4,10 @@ FactoryBot.define do
     user
     body { "just setting up my wklp" }
 
+    trait :invalid do
+      body { "" }
+    end
+
     trait :unformatted do
       body { " just setting up my wklp " }
     end
