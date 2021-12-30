3e22ef4ea4b31a410c5548ddb82642e00e705824
Rommy Gustiawan <rgcppp@gmail.com>
2021-12-30 17:35:23 +0700

    test: add missing test expectation
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/requests/api/v1/communities_controller_spec.rb
+++ spec/requests/api/v1/communities_controller_spec.rb
@@ -3,9 +3,20 @@ require "rails_helper"
 RSpec.describe Api::V1::CommunitiesController, type: :request do
   describe "POST /communities" do
     context "when user not logged in" do
+      before { post api_v1_communities_path }
+
       it "returns 401 status code" do
-        post api_v1_communities_path
         expect(response).to have_http_status(:unauthorized)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns unauthorized message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Unauthorized")
       end
     end
 
@@ -75,18 +86,42 @@ RSpec.describe Api::V1::CommunitiesController, type: :request do
 
   describe "PATCH /communities/:id" do
     context "when user not logged in" do
+      before { patch api_v1_community_path(1) }
+
       it "returns 401 status code" do
-        patch api_v1_community_path(1)
         expect(response).to have_http_status(:unauthorized)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns unauthorized message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Unauthorized")
       end
     end
 
     context "when community not exists" do
-      it "returns 404 status code" do
+      before do
         user = create(:user)
         token = JsonWebToken.encode({ id: user.id })
         patch api_v1_community_path(0), headers: { Authorization: "Bearer #{token}" }
+      end
+
+      it "returns 404 status code" do
         expect(response).to have_http_status(:not_found)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns not found message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Not Found")
       end
     end
 
--- spec/requests/api/v1/posts_controller_spec.rb
+++ spec/requests/api/v1/posts_controller_spec.rb
@@ -3,18 +3,42 @@ require "rails_helper"
 RSpec.describe Api::V1::PostsController, type: :request do
   describe "POST /communities/:community_id/posts" do
     context "when user not logged in" do
+      before { post api_v1_community_posts_path(1) }
+
       it "returns 401 status code" do
-        post api_v1_community_posts_path(1)
         expect(response).to have_http_status(:unauthorized)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns unauthorized message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Unauthorized")
       end
     end
 
     context "when community not exists" do
-      it "returns 404 status code" do
+      before do
         user = create(:user)
         token = JsonWebToken.encode({ id: user.id })
         post api_v1_community_posts_path(0), headers: { Authorization: "Bearer #{token}" }
+      end
+
+      it "returns 404 status code" do
         expect(response).to have_http_status(:not_found)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns not found message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Not Found")
       end
     end
 
@@ -104,18 +128,42 @@ RSpec.describe Api::V1::PostsController, type: :request do
 
   describe "PATCH /communities/:community_id/posts/:id" do
     context "when user not logged in" do
+      before { patch api_v1_community_post_path(1, 1) }
+
       it "returns 401 status code" do
-        patch api_v1_community_post_path(1, 1)
         expect(response).to have_http_status(:unauthorized)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns unauthorized message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Unauthorized")
       end
     end
 
     context "when community not exists" do
-      it "returns 404 status code" do
+      before do
         user = create(:user)
         token = JsonWebToken.encode({ id: user.id })
         patch api_v1_community_post_path(0, 1), headers: { Authorization: "Bearer #{token}" }
+      end
+
+      it "returns 404 status code" do
         expect(response).to have_http_status(:not_found)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns not found message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Not Found")
       end
     end
 
@@ -143,11 +191,24 @@ RSpec.describe Api::V1::PostsController, type: :request do
     end
 
     context "when post not exists" do
-      it "returns 404 status code" do
+      before do
         member = create(:member)
         token = JsonWebToken.encode({ id: member.user.id })
         patch api_v1_community_post_path(member.community, 0), headers: { Authorization: "Bearer #{token}" }
+      end
+
+      it "returns 404 status code" do
         expect(response).to have_http_status(:not_found)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns not found message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Not Found")
       end
     end
   end
