ede7023bc9238d92254ac5c62e8e9913e2ad0f44
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-01 07:58:44 +0700

    feat: add has many 'Reply' association on 'Comment'
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+  has_many :replies
 
   validates :post, presence: true
   validates :user, presence: true
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe Comment, type: :model do
   it { is_expected.to belong_to(:post) }
   it { is_expected.to belong_to(:user) }
+  it { is_expected.to have_many(:replies) }
   it { is_expected.to validate_presence_of(:post) }
   it { is_expected.to validate_presence_of(:user) }
   it { is_expected.to validate_length_of(:body).is_at_most(255) }
