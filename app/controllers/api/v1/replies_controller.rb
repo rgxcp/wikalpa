7b425033c3c71dca762e8bcb07b1feead65ce907
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-01 07:56:02 +0700

    feat: POST /comments/:id/replies returns 404 status code when comment not exists
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/replies_controller.rb
+++ app/controllers/api/v1/replies_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::RepliesController < ApplicationController
   before_action :authenticate_request, only: :create
 
   def create
+    comment = Comment.find(params[:comment_id])
   end
 end
