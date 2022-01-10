7474648775c8191bd762e184353608be05bbd5b1
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-10 11:39:23 +0700

    feat: GET /users/:id/communities returns not found response when communities not exist
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/user/communities_controller.rb
+++ app/controllers/api/v1/user/communities_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::User::CommunitiesController < ApplicationController
   def index
     user = User.find(params[:user_id])
+
+    communities = user.communities
+
+    not_found_response unless communities.size.positive?
   end
 end
