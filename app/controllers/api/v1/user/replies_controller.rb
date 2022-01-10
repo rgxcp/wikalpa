5279aa2fe0ee714250c319ecfd95a59a5cc803a1
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-10 11:56:05 +0700

    feat: GET /users/:id/replies returns not found response when replies not exist
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/user/replies_controller.rb
+++ app/controllers/api/v1/user/replies_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::User::RepliesController < ApplicationController
   def index
     user = User.find(params[:user_id])
+
+    replies = user.replies
+
+    not_found_response unless replies.size.positive?
   end
 end
