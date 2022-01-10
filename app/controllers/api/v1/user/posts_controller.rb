920f2116d387254b7a9a5f6fb0cf96dae053f9c1
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-10 11:47:48 +0700

    feat: GET /users/:id/posts returns not found response when posts not exist
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/user/posts_controller.rb
+++ app/controllers/api/v1/user/posts_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::User::PostsController < ApplicationController
   def index
     user = User.find(params[:user_id])
+
+    posts = user.posts
+
+    not_found_response unless posts.size.positive?
   end
 end
