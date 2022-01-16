51689149fbc2c558cc88dfc7b00f2f6b031b2555
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-16 16:19:47 +0700

    chore: set sidekiq as background job adapter
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,8 @@ module Wikalpa
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Background job adapter
+    config.active_job.queue_adapter = :sidekiq
   end
 end
