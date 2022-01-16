3f11861120ad160f3ff98992335a6ef673f8756b
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-16 15:36:50 +0700

    feat: add has many 'Visitor' polymorphic association on 'User'
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :members
   has_many :posts
   has_many :replies
+  has_many :visitors, as: :visitable
   has_many :communities, through: :members
 
   validate :password_equality
