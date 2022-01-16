31d7d060a9a1fb13f992d69ff4a553298284bc07
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-16 18:48:15 +0700

    feat: VisitorWorker.perform adds visitable visitor by one
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/workers/visitor_worker.rb
+++ app/workers/visitor_worker.rb
@@ -2,5 +2,7 @@ class VisitorWorker
   include Sidekiq::Worker
 
   def perform(visitable_type, visitable_id, user_id)
+    visitable = visitable_type.constantize.find(visitable_id)
+    visitable.visitors.create(user_id: user_id) unless visitable.user_id == user_id
   end
 end
--- spec/workers/visitor_worker_spec.rb
+++ spec/workers/visitor_worker_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe VisitorWorker, type: :worker do
       expect(VisitorWorker).to have_enqueued_sidekiq_job("VisitableType", 1, 1)
     end
   end
+
+  describe "#perform" do
+    it "adds visitable visitor by one" do
+      community = create(:community)
+      user1 = create(:user)
+      user2 = create(:user)
+      visitable = create(:post, community: community, user: user2)
+      VisitorWorker.new.perform(visitable.class.name, visitable.id, user1.id)
+      visitor_count = Visitor.where(user: user1, visitable: visitable).count
+      expect(visitor_count).to eq(1)
+    end
+  end
 end
