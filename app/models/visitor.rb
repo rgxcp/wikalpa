dd8b85d1a38fb0646d291ba907091b12a78ff57a
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-16 15:28:03 +0700

    feat: validate visiting self visitable
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -2,5 +2,12 @@ class Visitor < ApplicationRecord
   belongs_to :user
   belongs_to :visitable, polymorphic: true
 
+  validate :visiting_self_visitable, if: -> { visitable.present? && user.present? }
   validates :user, presence: true
+
+  private
+
+  def visiting_self_visitable
+    errors.add(:visitable) if visitable.user_id == user.id
+  end
 end
