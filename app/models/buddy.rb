0efc6cd03845a32a47a42b0966eb46fb40413b5c
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-16 15:29:04 +0700

    refactor: only validate adding self as buddy when both buddy & user present
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/buddy.rb
+++ app/models/buddy.rb
@@ -2,7 +2,7 @@ class Buddy < ApplicationRecord
   belongs_to :user
   belongs_to :buddy, class_name: "User"
 
-  validate :adding_self_as_buddy
+  validate :adding_self_as_buddy, if: -> { buddy.present? && user.present? }
   validates :user, presence: true
   validates :buddy, presence: true
   validates :buddy, uniqueness: { scope: :user_id }
@@ -10,6 +10,6 @@ class Buddy < ApplicationRecord
   private
 
   def adding_self_as_buddy
-    errors.add(:buddy) if buddy == user
+    errors.add(:buddy) if buddy.id == user.id
   end
 end
