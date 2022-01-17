ad1fc84e96e1e838386b2df6a2d86e72c71d4916
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-17 18:13:25 +0700

    feat: GET /communities/:id/visitors returns not found response when visitors not exist
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/community/visitors_controller.rb
+++ app/controllers/api/v1/community/visitors_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Community::VisitorsController < ApplicationController
   def index
     community = Community.find(params[:community_id])
+
+    visitors = community.visitors
+
+    not_found_response unless visitors.size.positive?
   end
 end
