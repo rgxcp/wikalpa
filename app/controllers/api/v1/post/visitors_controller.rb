4c9ef3c1ec2871a46d60d380c682afcc6c2b7098
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-17 18:56:29 +0700

    feat: GET /posts/:id/visitors returns not found response when visitors not exist
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/post/visitors_controller.rb
+++ app/controllers/api/v1/post/visitors_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Post::VisitorsController < ApplicationController
   def index
     post = Post.find(params[:post_id])
+
+    visitors = post.visitors
+
+    not_found_response unless visitors.size.positive?
   end
 end
