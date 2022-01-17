bd141738ebf6fb44174e3bc97f0b2d3fddee547f
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-17 18:15:40 +0700

    feat: GET /communities/:id/visitors returns ok response when visitors exist
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/requests/api/v1/community/visitors_controller_spec.rb
+++ spec/requests/api/v1/community/visitors_controller_spec.rb
@@ -40,5 +40,33 @@ RSpec.describe Api::V1::Community::VisitorsController, type: :request do
         expect(result["message"]).to eq("Not Found")
       end
     end
+
+    context "when visitors exist" do
+      before do
+        user = create(:user)
+        community = create(:community)
+        create(:visitor, user: user, visitable: community)
+        get api_v1_community_visitors_path(community)
+      end
+
+      it "returns 200 status code" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "returns true success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be true
+      end
+
+      it "returns ok message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("OK")
+      end
+
+      it "returns visitors data" do
+        result = JSON.parse(response.body)
+        expect(result["data"]["visitors"]).not_to be_empty
+      end
+    end
   end
 end
