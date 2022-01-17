70196109d6c8cf278b607ad02954a51375ee31d0
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-17 12:04:42 +0700

    revert: validate visiting self visitable
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/workers/visitor_worker.rb
+++ app/workers/visitor_worker.rb
@@ -3,6 +3,6 @@ class VisitorWorker
 
   def perform(visitable_type, visitable_id, user_id)
     visitable = visitable_type.constantize.find(visitable_id)
-    visitable.visitors.create(user_id: user_id) unless visitable.user_id == user_id
+    visitable.visitors.create(user_id: user_id)
   end
 end
--- spec/models/visitor_spec.rb
+++ spec/models/visitor_spec.rb
@@ -4,17 +4,4 @@ RSpec.describe Visitor, type: :model do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:visitable) }
   it { is_expected.to validate_presence_of(:user) }
-
-  describe "custom validation" do
-    context "when visiting self visitable" do
-      it "is not valid" do
-        community = create(:community)
-        user = create(:user)
-        post = create(:post, community: community, user: user)
-        visitor = build(:visitor, user: user, visitable: post)
-        visitor.validate
-        expect(visitor).not_to be_valid
-      end
-    end
-  end
 end
