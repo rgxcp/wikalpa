5e13e4fdda3165eab8a2b3cc1e1ca3d340e977ae
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-22 18:59:04 +0700

    feat: GET /users/:id/likes returns not found response when likes not exist
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/user/likes_controller.rb
+++ app/controllers/api/v1/user/likes_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::User::LikesController < ApplicationController
   def index
     user = User.find(params[:user_id])
+
+    likes = user.likes
+
+    not_found_response unless likes.size.positive?
   end
 end
