0500063a95040e8570bd627b1f06e52c689c026a
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-23 08:04:09 +0700

    refactor: move test specs from parent to its own module
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/requests/api/v1/post/likes_controller_spec.rb
+++ spec/requests/api/v1/post/likes_controller_spec.rb
@@ -70,4 +70,110 @@ RSpec.describe Api::V1::Post::LikesController, type: :request do
       end
     end
   end
+
+  describe "POST /posts/:post_id/likes" do
+    context "when user not logged in" do
+      before { post api_v1_post_likes_path(1) }
+
+      it "returns 401 status code" do
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns unauthorized message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Unauthorized")
+      end
+    end
+
+    context "when post not exists" do
+      before do
+        user = create(:user)
+        token = JsonWebToken.encode({ id: user.id })
+        post api_v1_post_likes_path(0), headers: { Authorization: "Bearer #{token}" }
+      end
+
+      it "returns 404 status code" do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns not found message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Not Found")
+      end
+    end
+
+    context "when entity invalid" do
+      before do
+        community = create(:community)
+        user = create(:user)
+        post = create(:post, community: community, user: user)
+        create(:like, user: user, likeable: post)
+        token = JsonWebToken.encode({ id: user.id })
+        post api_v1_post_likes_path(post), headers: { Authorization: "Bearer #{token}" }
+      end
+
+      it "returns 422 status code" do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns unprocessable entity message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Unprocessable Entity")
+      end
+
+      it "returns validation errors" do
+        result = JSON.parse(response.body)
+        expect(result["errors"].size).to be_positive
+      end
+    end
+
+    context "when entity valid" do
+      before do
+        community = create(:community)
+        user = create(:user)
+        post = create(:post, community: community, user: user)
+        token = JsonWebToken.encode({ id: user.id })
+        post api_v1_post_likes_path(post), headers: { Authorization: "Bearer #{token}" }
+      end
+
+      it "returns 201 status code" do
+        expect(response).to have_http_status(:created)
+      end
+
+      it "returns true success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be true
+      end
+
+      it "returns created message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Created")
+      end
+
+      it "returns like data" do
+        result = JSON.parse(response.body)
+        expect(result["data"]["like"]).not_to be_empty
+      end
+
+      it "returns post as the likeable type" do
+        result = JSON.parse(response.body)
+        expect(result["data"]["like"]["likeable_type"]).to eq("Post")
+      end
+    end
+  end
 end
