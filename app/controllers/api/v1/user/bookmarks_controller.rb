5ff86df1c494310f3942827e473349d5713a1ae7
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-23 09:16:31 +0700

    feat: GET /users/:id/bookmarks returns forbidden response when viewing someone else bookmarks
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/user/bookmarks_controller.rb
+++ app/controllers/api/v1/user/bookmarks_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::User::BookmarksController < ApplicationController
   before_action :authenticate_request, only: :index
 
   def index
+    forbidden_response unless @auth_user.id == params[:user_id].to_i
   end
 end
