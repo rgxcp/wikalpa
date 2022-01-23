52db43295edd32e72c3756e05cc679764c9c6fd0
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-23 07:43:18 +0700

    feat: validate uniqueness of 'Bookmark' user scoped to bookmarkable_type & bookmarkable_id
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,4 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :bookmarkable, polymorphic: true
 
   validates :user, presence: true
+  validates :user, uniqueness: { scope: [:bookmarkable_type, :bookmarkable_id] }
 end
--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,7 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Bookmark, type: :model do
+  subject(:bookmark) { build(:bookmark) }
+
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:bookmarkable) }
   it { is_expected.to validate_presence_of(:user) }
+  it { is_expected.to validate_uniqueness_of(:user).scoped_to([:bookmarkable_type, :bookmarkable_id]) }
 end
