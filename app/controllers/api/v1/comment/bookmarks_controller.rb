1e17767c156ac5c0d613c7822c1c02761d65ee56
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-23 08:35:23 +0700

    feat: POST /comments/:id/bookmarks returns unprocessable entity response when entity invalid
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/comment/bookmarks_controller.rb
+++ app/controllers/api/v1/comment/bookmarks_controller.rb
@@ -3,5 +3,9 @@ class Api::V1::Comment::BookmarksController < ApplicationController
 
   def create
     comment = Comment.find(params[:comment_id])
+
+    bookmark = comment.bookmarks.build(user: @auth_user)
+
+    unprocessable_entity_response(errors: bookmark.errors.messages) unless bookmark.save
   end
 end
--- spec/requests/api/v1/comment/bookmarks_controller_spec.rb
+++ spec/requests/api/v1/comment/bookmarks_controller_spec.rb
@@ -41,5 +41,36 @@ RSpec.describe Api::V1::Comment::BookmarksController, type: :request do
         expect(result["message"]).to eq("Not Found")
       end
     end
+
+    context "when entity invalid" do
+      before do
+        community = create(:community)
+        user = create(:user)
+        post = create(:post, community: community, user: user)
+        comment = create(:comment, post: post, user: user)
+        create(:bookmark, user: user, bookmarkable: comment)
+        token = JsonWebToken.encode({ id: user.id })
+        post api_v1_comment_bookmarks_path(comment), headers: { Authorization: "Bearer #{token}" }
+      end
+
+      it "returns 422 status code" do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns unprocessable entity message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Unprocessable Entity")
+      end
+
+      it "returns validation errors" do
+        result = JSON.parse(response.body)
+        expect(result["errors"].size).to be_positive
+      end
+    end
   end
 end
