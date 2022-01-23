fe147c139303c219635e27456c0ad00be93c67d7
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-23 07:49:35 +0700

    feat: add has many 'Bookmark' polymorphic association on 'Comment'
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe Comment, type: :model do
   it { is_expected.to belong_to(:post) }
   it { is_expected.to belong_to(:user) }
+  it { is_expected.to have_many(:bookmarks) }
   it { is_expected.to have_many(:likes) }
   it { is_expected.to have_many(:replies) }
   it { is_expected.to validate_presence_of(:post) }
