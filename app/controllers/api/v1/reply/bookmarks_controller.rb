3ff82ee6e8bbba6e3042a46d1eaa8abfb4c2b268
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-23 08:44:44 +0700

    feat: POST /replies/:id/bookmarks returns not found response when reply not exists
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/reply/bookmarks_controller.rb
+++ app/controllers/api/v1/reply/bookmarks_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::Reply::BookmarksController < ApplicationController
   before_action :authenticate_request, only: :create
 
   def create
+    reply = Reply.find(params[:reply_id])
   end
 end
