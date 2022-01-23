b2586328a5554e4d830e3cdb9aaad3c933518978
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-23 08:47:30 +0700

    feat: POST /replies/:id/bookmarks returns created response when entity valid
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/requests/api/v1/reply/bookmarks_controller_spec.rb
+++ spec/requests/api/v1/reply/bookmarks_controller_spec.rb
@@ -73,5 +73,41 @@ RSpec.describe Api::V1::Reply::BookmarksController, type: :request do
         expect(result["errors"].size).to be_positive
       end
     end
+
+    context "when entity valid" do
+      before do
+        community = create(:community)
+        user = create(:user)
+        post = create(:post, community: community, user: user)
+        comment = create(:comment, post: post, user: user)
+        reply = create(:reply, comment: comment, user: user)
+        token = JsonWebToken.encode({ id: user.id })
+        post api_v1_reply_bookmarks_path(reply), headers: { Authorization: "Bearer #{token}" }
+      end
+
+      it "returns 201 status code" do
+        expect(response).to have_http_status(:created)
+      end
+
+      it "returns true success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be true
+      end
+
+      it "returns created message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Created")
+      end
+
+      it "returns bookmark data" do
+        result = JSON.parse(response.body)
+        expect(result["data"]["bookmark"]).not_to be_empty
+      end
+
+      it "returns reply as the bookmarkable type" do
+        result = JSON.parse(response.body)
+        expect(result["data"]["bookmark"]["bookmarkable_type"]).to eq("Reply")
+      end
+    end
   end
 end
