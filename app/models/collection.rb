d1440143c7b27e906936ca325dd9834315f2d316
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-26 12:41:02 +0700

    feat: validate presence of name on 'Collection'
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,4 +2,5 @@ class Collection < ApplicationRecord
   belongs_to :user
 
   validates :user, presence: true
+  validates :name, presence: true
 end
--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -3,4 +3,5 @@ require "rails_helper"
 RSpec.describe Collection, type: :model do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:user) }
+  it { is_expected.to validate_presence_of(:name) }
 end
