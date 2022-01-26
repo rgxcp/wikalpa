30decbd011f0566490c0676038e0d14621606ae7
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-26 12:40:47 +0700

    feat: validate presence of user on 'Collection'
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,5 @@
 class Collection < ApplicationRecord
   belongs_to :user
+
+  validates :user, presence: true
 end
--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -2,4 +2,5 @@ require "rails_helper"
 
 RSpec.describe Collection, type: :model do
   it { is_expected.to belong_to(:user) }
+  it { is_expected.to validate_presence_of(:user) }
 end
