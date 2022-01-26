a5f0d0c39e03e46225465e74991f054814009b86
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-26 13:16:37 +0700

    feat: add polymorphic belongs to collectable association on 'Collection'
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/collection_item.rb
+++ app/models/collection_item.rb
@@ -1,3 +1,4 @@
 class CollectionItem < ApplicationRecord
   belongs_to :collection
+  belongs_to :collectable, polymorphic: true
 end
--- spec/models/collection_item_spec.rb
+++ spec/models/collection_item_spec.rb
@@ -2,4 +2,5 @@ require "rails_helper"
 
 RSpec.describe CollectionItem, type: :model do
   it { is_expected.to belong_to(:collection) }
+  it { is_expected.to belong_to(:collectable) }
 end
