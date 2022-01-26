922d67deb11efe9f00628feda55d52460590c743
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-26 18:44:29 +0700

    chore: add collection_items_attributes attribute on collection factory
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/factories/collections.rb
+++ spec/factories/collections.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :collection do
     user
     name { "Must Join Communities" }
+    collection_items_attributes { [{ collectable: association(:community) }] }
 
     trait :unformatted do
       name { " Must Join Communities " }
