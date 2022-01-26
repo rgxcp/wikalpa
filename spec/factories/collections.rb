6fa7025668251e2e1a07b8aa5b955dbb9a9ce062
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-26 13:04:24 +0700

    chore: add unformatted trait on collection factory
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/factories/collections.rb
+++ spec/factories/collections.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :collection do
     user
     name { "Must join communities" }
+
+    trait :unformatted do
+      name { " Must join communities " }
+    end
   end
 end
