17950cfd6fc31318a5e089f9d5baa284e7a73e73
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-26 13:33:03 +0700

    feat: validate presence of collection on 'CollectionItem'
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/collection_item.rb
+++ app/models/collection_item.rb
@@ -1,4 +1,6 @@
 class CollectionItem < ApplicationRecord
   belongs_to :collection
   belongs_to :collectable, polymorphic: true
+
+  validates :collection, presence: true
 end
--- spec/models/collection_item_spec.rb
+++ spec/models/collection_item_spec.rb
@@ -3,4 +3,5 @@ require "rails_helper"
 RSpec.describe CollectionItem, type: :model do
   it { is_expected.to belong_to(:collection) }
   it { is_expected.to belong_to(:collectable) }
+  it { is_expected.to validate_presence_of(:collection) }
 end
