038d1be6a6e4fce88e6e77c9817bb90b52698555
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-27 10:00:12 +0700

    feat: POST /collections returns unprocessable entity response when entity invalid
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -2,5 +2,15 @@ class Api::V1::CollectionsController < ApplicationController
   before_action :authenticate_request, only: :create
 
   def create
+    collection = Collection.new(collection_params)
+    collection.user = @auth_user
+
+    unprocessable_entity_response(errors: collection.errors.messages) unless collection.save
+  end
+
+  private
+
+  def collection_params
+    params.require(:collection).permit(:name, :collection_items_attributes)
   end
 end
