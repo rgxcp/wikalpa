afe7830a12952f0b197ecaa90bdde268edd3bb76
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-27 09:58:49 +0700

    chore: add invalid trait on collection factory
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/factories/collections.rb
+++ spec/factories/collections.rb
@@ -4,6 +4,11 @@ FactoryBot.define do
     name { "Must Join Communities" }
     collection_items_attributes { [{ collectable: association(:community) }] }
 
+    trait :invalid do
+      name { "" }
+      collection_items_attributes { [] }
+    end
+
     trait :unformatted do
       name { " Must Join Communities " }
     end
