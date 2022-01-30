6d6d478760cce2e9045d279f178a767650e271ee
Rommy Gustiawan <rgcppp@gmail.com>
2022-01-30 14:50:20 +0700

    feat: validate inclusion of visitable type on 'Visitor'
    ALLOWED TYPES: Collection, Community, Post, User
    
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -3,4 +3,5 @@ class Visitor < ApplicationRecord
   belongs_to :visitable, polymorphic: true
 
   validates :user, presence: true
+  validates :visitable_type, inclusion: { in: ["Collection", "Community", "Post", "User"] }
 end
--- spec/models/visitor_spec.rb
+++ spec/models/visitor_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Visitor, type: :model do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:visitable) }
   it { is_expected.to validate_presence_of(:user) }
+  it { is_expected.to validate_inclusion_of(:visitable_type).in_array(["Collection", "Community", "Post", "User"]) }
 end
