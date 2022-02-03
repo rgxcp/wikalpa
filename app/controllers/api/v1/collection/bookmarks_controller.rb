3967a8a735c2a1e7ce6fd3273e9ab7062aa35018
Rommy Gustiawan <rgcppp@gmail.com>
2022-02-03 08:00:37 +0700

    feat: POST /collections/:id/bookmarks returns unprocessable entity response when entity invalid
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/collection/bookmarks_controller.rb
+++ app/controllers/api/v1/collection/bookmarks_controller.rb
@@ -3,5 +3,9 @@ class Api::V1::Collection::BookmarksController < ApplicationController
 
   def create
     collection = Collection.find(params[:collection_id])
+
+    bookmark = collection.bookmarks.build(user: @auth_user)
+
+    unprocessable_entity_response(errors: bookmark.errors.messages) unless bookmark.save
   end
 end
