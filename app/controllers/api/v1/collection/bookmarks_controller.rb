77da2036a64095744fe78d527fc91d9ab391d8dc
Rommy Gustiawan <rgcppp@gmail.com>
2022-02-03 08:08:52 +0700

    refactor: inherit 'Api::V1::BookmarksController' class instead of 'ApplicationController'
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/collection/bookmarks_controller.rb
+++ app/controllers/api/v1/collection/bookmarks_controller.rb
@@ -1,15 +1,7 @@
-class Api::V1::Collection::BookmarksController < ApplicationController
-  before_action :authenticate_request, only: :create
+class Api::V1::Collection::BookmarksController < Api::V1::BookmarksController
+  private
 
-  def create
-    collection = Collection.find(params[:collection_id])
-
-    bookmark = collection.bookmarks.build(user: @auth_user)
-
-    if bookmark.save
-      created_response(data: { bookmark: bookmark })
-    else
-      unprocessable_entity_response(errors: bookmark.errors.messages)
-    end
+  def set_bookmarkable
+    @bookmarkable = Collection.find(params[:collection_id])
   end
 end
