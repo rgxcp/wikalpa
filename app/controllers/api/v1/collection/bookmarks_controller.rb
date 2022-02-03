801b6316852b92007f9ec9e53e294977b019ddb7
Rommy Gustiawan <rgcppp@gmail.com>
2022-02-03 07:57:44 +0700

    feat: POST /collections/:id/bookmarks returns not found response when collection not exists
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/collection/bookmarks_controller.rb
+++ app/controllers/api/v1/collection/bookmarks_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::Collection::BookmarksController < ApplicationController
   before_action :authenticate_request, only: :create
 
   def create
+    collection = Collection.find(params[:collection_id])
   end
 end
--- spec/requests/api/v1/collection/bookmarks_controller_spec.rb
+++ spec/requests/api/v1/collection/bookmarks_controller_spec.rb
@@ -19,5 +19,28 @@ RSpec.describe Api::V1::Collection::BookmarksController, type: :request do
         expect(result["message"]).to eq("Unauthorized")
       end
     end
+
+    context "when collection not exists" do
+      before do
+        user = create(:user)
+        token = JsonWebToken.encode({ id: user.id })
+        headers = { Authorization: "Bearer #{token}" }
+        post api_v1_collection_bookmarks_path(0), headers: headers
+      end
+
+      it "returns 404 status code" do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns not found message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Not Found")
+      end
+    end
   end
 end
