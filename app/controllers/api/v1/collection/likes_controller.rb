e3045dc4676c6eb0ca42ca5c9d7f79bb19637003
Rommy Gustiawan <rgcppp@gmail.com>
2022-02-05 10:44:20 +0700

    feat: POST /collections/:id/likes returns unprocessable entity response when entity invalid
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/collection/likes_controller.rb
+++ app/controllers/api/v1/collection/likes_controller.rb
@@ -3,5 +3,9 @@ class Api::V1::Collection::LikesController < ApplicationController
 
   def create
     collection = Collection.find(params[:collection_id])
+
+    like = collection.likes.build(user: @auth_user)
+
+    unprocessable_entity_response(errors: like.errors.messages) unless like.save
   end
 end
