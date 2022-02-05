0170994fe4512ea4a4e5c7aa99f590f2a934f601
Rommy Gustiawan <rgcppp@gmail.com>
2022-02-05 10:40:04 +0700

    feat: POST /collections/:id/likes returns not found response when collection not exists
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/collection/likes_controller.rb
+++ app/controllers/api/v1/collection/likes_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::Collection::LikesController < ApplicationController
   before_action :authenticate_request, only: :create
 
   def create
+    collection = Collection.find(params[:collection_id])
   end
 end
