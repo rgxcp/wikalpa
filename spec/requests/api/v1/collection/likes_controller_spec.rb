483a8c965b50ecfc666c0179c902d306242c6d5d
Rommy Gustiawan <rgcppp@gmail.com>
2022-02-06 07:02:57 +0700

    feat: GET /collections/:id/likes retrieve all likes from a collection
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/requests/api/v1/collection/likes_controller_spec.rb
+++ spec/requests/api/v1/collection/likes_controller_spec.rb
@@ -1,6 +1,75 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::Collection::LikesController, type: :request do
+  describe "GET /collections/:collection_id/likes" do
+    context "when collection not exists" do
+      before { get api_v1_collection_likes_path(0) }
+
+      it "returns 404 status code" do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns not found message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Not Found")
+      end
+    end
+
+    context "when likes not exist" do
+      before do
+        collection = create(:collection)
+        get api_v1_collection_likes_path(collection)
+      end
+
+      it "returns 404 status code" do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be false
+      end
+
+      it "returns not found message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Not Found")
+      end
+    end
+
+    context "when likes exist" do
+      before do
+        user = create(:user)
+        collection = create(:collection, user: user)
+        create(:like, user: user, likeable: collection)
+        get api_v1_collection_likes_path(collection)
+      end
+
+      it "returns 200 status code" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "returns true success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be true
+      end
+
+      it "returns ok message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("OK")
+      end
+
+      it "returns likes data" do
+        result = JSON.parse(response.body)
+        expect(result["data"]["likes"]).not_to be_empty
+      end
+    end
+  end
+
   describe "POST /collections/:collection_id/likes" do
     context "when user not logged in" do
       before { post api_v1_collection_likes_path(1) }
