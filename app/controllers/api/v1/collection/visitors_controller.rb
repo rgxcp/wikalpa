39456b554548baf3cacf450854773608ecaa103e
Rommy Gustiawan <rgcppp@gmail.com>
2022-02-07 09:56:48 +0700

    feat: GET /collections/:id/visitors returns not found response when visitors not exist
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/collection/visitors_controller.rb
+++ app/controllers/api/v1/collection/visitors_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Collection::VisitorsController < ApplicationController
   def index
     collection = Collection.find(params[:collection_id])
+
+    visitors = collection.visitors
+
+    not_found_response unless visitors.size.positive?
   end
 end
