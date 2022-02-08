1e90a0be2e88538939ba3fb0dffc6d64933909d6
Rommy Gustiawan <rgcppp@gmail.com>
2022-02-08 07:14:36 +0700

    feat: GET /comments/:id/collections returns not found response when collections not exist
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/comment/collections_controller.rb
+++ app/controllers/api/v1/comment/collections_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Comment::CollectionsController < ApplicationController
   def index
     comment = Comment.find(params[:comment_id])
+
+    collections = comment.collections
+
+    not_found_response unless collections.size.positive?
   end
 end
