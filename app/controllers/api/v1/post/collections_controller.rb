0293b0a0e83f5af4b17702dd76882075b0ad1937
Rommy Gustiawan <rgcppp@gmail.com>
2022-03-13 13:37:31 +0700

    feat: GET /posts/:id/collections returns not found response when collections not exist
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/post/collections_controller.rb
+++ app/controllers/api/v1/post/collections_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Post::CollectionsController < ApplicationController
   def index
     post = Post.find(params[:post_id])
+
+    collections = post.collections
+
+    not_found_response unless collections.size.positive?
   end
 end
