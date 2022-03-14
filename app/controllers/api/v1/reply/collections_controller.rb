568db7fc72dea993332384690968fe676ed09e8f
Rommy Gustiawan <rgcppp@gmail.com>
2022-03-14 13:26:14 +0700

    feat: GET /replies/:id/collections returns ok response when collections exist
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/reply/collections_controller.rb
+++ app/controllers/api/v1/reply/collections_controller.rb
@@ -4,6 +4,10 @@ class Api::V1::Reply::CollectionsController < ApplicationController
 
     collections = reply.collections
 
-    not_found_response unless collections.size.positive?
+    if collections.size.positive?
+      ok_response(data: { collections: collections })
+    else
+      not_found_response
+    end
   end
 end
--- spec/requests/api/v1/reply/collections_controller_spec.rb
+++ spec/requests/api/v1/reply/collections_controller_spec.rb
@@ -40,5 +40,32 @@ RSpec.describe Api::V1::Reply::CollectionsController, type: :request do
         expect(result["message"]).to eq("Not Found")
       end
     end
+
+    context "when collections exist" do
+      before do
+        reply = create(:reply)
+        create(:collection, collection_items_attributes: [{ collectable: reply }])
+        get api_v1_reply_collections_path(reply)
+      end
+
+      it "returns 200 status code" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "returns true success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be(true)
+      end
+
+      it "returns ok message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("OK")
+      end
+
+      it "returns collections data" do
+        result = JSON.parse(response.body)
+        expect(result["data"]["collections"]).not_to be_empty
+      end
+    end
   end
 end
