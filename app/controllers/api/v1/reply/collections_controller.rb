ac869fdaade00f0a2e9840a89bd6a3ee3b767e5f
Rommy Gustiawan <rgcppp@gmail.com>
2022-03-14 13:25:35 +0700

    feat: GET /replies/:id/collections returns not found response when collections not exist
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/reply/collections_controller.rb
+++ app/controllers/api/v1/reply/collections_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Reply::CollectionsController < ApplicationController
   def index
     reply = Reply.find(params[:reply_id])
+
+    collections = reply.collections
+
+    not_found_response unless collections.size.positive?
   end
 end
