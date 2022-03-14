d0b26e4e339149bb5d0ff9a96d17d6e1a7208bef
Rommy Gustiawan <rgcppp@gmail.com>
2022-03-15 04:27:27 +0700

    feat: GET /users/:id/collections returns not found response when collections not exist
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/user/collections_controller.rb
+++ app/controllers/api/v1/user/collections_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::User::CollectionsController < ApplicationController
   def index
     user = User.find(params[:user_id])
+
+    collections = user.collections
+
+    not_found_response unless collections.size.positive?
   end
 end
