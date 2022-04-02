49e1714dd2c09a5ab876f4172489f68bfd82d61d
Rommy Gustiawan <rgcppp@gmail.com>
2022-04-02 21:54:32 +0700

    feat: add belongs to 'User' association on 'Downvote'
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/downvote.rb
+++ app/models/downvote.rb
@@ -1,3 +1,4 @@
 class Downvote < ApplicationRecord
   belongs_to :downvoteable, polymorphic: true
+  belongs_to :user
 end
--- spec/models/downvote_spec.rb
+++ spec/models/downvote_spec.rb
@@ -2,4 +2,5 @@ require "rails_helper"
 
 RSpec.describe Downvote, type: :model do
   it { is_expected.to belong_to(:downvoteable) }
+  it { is_expected.to belong_to(:user) }
 end
