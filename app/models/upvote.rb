9b74a23678432879c899b85966e7fde32b2bbf63
Rommy Gustiawan <rgcppp@gmail.com>
2022-04-03 11:10:20 +0700

    feat: delete 'Downvote' if exists before create 'Upvote'
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -5,4 +5,13 @@ class Upvote < ApplicationRecord
   validates :user, presence: true
   validates :user, uniqueness: { scope: [:upvoteable_type, :upvoteable_id] }
   validates :upvoteable_type, inclusion: { in: ["Collection", "Comment", "Post", "Reply"] }
+
+  before_create :delete_downvote_if_exists
+
+  private
+
+  def delete_downvote_if_exists
+    downvote = Downvote.find_by(user: user, downvoteable: upvoteable)
+    downvote.destroy if downvote
+  end
 end
--- spec/models/upvote_spec.rb
+++ spec/models/upvote_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Upvote, type: :model do
   it { is_expected.to validate_presence_of(:user) }
   it { is_expected.to validate_uniqueness_of(:user).scoped_to([:upvoteable_type, :upvoteable_id]) }
   it { is_expected.to validate_inclusion_of(:upvoteable_type).in_array(["Collection", "Comment", "Post", "Reply"]) }
+
+  describe "#before_create" do
+    context "when upvoteable is downvoted" do
+      it "deletes the downvote first" do
+        user = create(:user)
+        post = create(:post, user: user)
+        create(:downvote, user: user, downvoteable: post)
+        create(:upvote, user: user, upvoteable: post)
+        expect(Downvote.exists?(user: user, downvoteable: post)).to be(false)
+      end
+    end
+  end
 end
