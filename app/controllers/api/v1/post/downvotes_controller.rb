7c442ac20724df425bf0c22314e5a97092fbcc3c
Rommy Gustiawan <rgcppp@gmail.com>
2022-04-13 19:13:10 +0700

    feat: POST /posts/:id/downvotes returns not found response when post not exists
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/post/downvotes_controller.rb
+++ app/controllers/api/v1/post/downvotes_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::Post::DownvotesController < ApplicationController
   before_action :authenticate_request, only: :create
 
   def create
+    post = Post.find(params[:post_id])
   end
 end
