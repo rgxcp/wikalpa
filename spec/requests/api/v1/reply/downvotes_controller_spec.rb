856a474dddc26c61494e9fa0a96d2f319eb89ec9
Rommy Gustiawan <rgcppp@gmail.com>
2022-04-14 19:42:57 +0700

    feat: POST /replies/:id/downvotes returns created response when entity valid
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/requests/api/v1/reply/downvotes_controller_spec.rb
+++ spec/requests/api/v1/reply/downvotes_controller_spec.rb
@@ -75,5 +75,42 @@ RSpec.describe Api::V1::Reply::DownvotesController, type: :request do
         expect(result["errors"].size).to be_positive
       end
     end
+
+    context "when entity valid" do
+      before do
+        community = create(:community)
+        user = create(:user)
+        post = create(:post, community: community, user: user)
+        comment = create(:comment, post: post, user: user)
+        reply = create(:reply, comment: comment, user: user)
+        token = JsonWebToken.encode({ id: user.id })
+        headers = { Authorization: "Bearer #{token}" }
+        post api_v1_reply_downvotes_path(reply), headers: headers
+      end
+
+      it "returns 201 status code" do
+        expect(response).to have_http_status(:created)
+      end
+
+      it "returns true success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be(true)
+      end
+
+      it "returns created message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Created")
+      end
+
+      it "returns downvote data" do
+        result = JSON.parse(response.body)
+        expect(result["data"]["downvote"]).not_to be_empty
+      end
+
+      it "returns reply as the downvoteable type" do
+        result = JSON.parse(response.body)
+        expect(result["data"]["downvote"]["downvoteable_type"]).to eq("Reply")
+      end
+    end
   end
 end
