3a9ba6fc90410860b759cff447bc52b41b200edf
Rommy Gustiawan <rgcppp@gmail.com>
2022-04-14 19:42:04 +0700

    feat: POST /replies/:id/downvotes returns unprocessable entity response when entity invalid
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/reply/downvotes_controller.rb
+++ app/controllers/api/v1/reply/downvotes_controller.rb
@@ -3,5 +3,9 @@ class Api::V1::Reply::DownvotesController < ApplicationController
 
   def create
     reply = Reply.find(params[:reply_id])
+
+    downvote = reply.downvotes.build(user: @auth_user)
+
+    unprocessable_entity_response(errors: downvote.errors.messages) unless downvote.save
   end
 end
