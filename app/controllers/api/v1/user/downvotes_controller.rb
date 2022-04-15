4055c582dd0892a6016483c5d1cd649821cf4d05
Rommy Gustiawan <rgcppp@gmail.com>
2022-04-16 05:56:16 +0700

    feat: GET /users/:id/downvotes returns forbidden response when viewing someone else downvotes
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/user/downvotes_controller.rb
+++ app/controllers/api/v1/user/downvotes_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::User::DownvotesController < ApplicationController
   before_action :authenticate_request, only: :index
 
   def index
+    return forbidden_response unless @auth_id == params[:user_id].to_i
   end
 end
