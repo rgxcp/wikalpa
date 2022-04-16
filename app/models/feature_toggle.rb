bdac60b76b0f011854ba316738840d6bf04e8383
Rommy Gustiawan <rgcppp@gmail.com>
2022-04-16 09:57:15 +0700

    feat: validate length of name is at most 50 on feature toggle
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/feature_toggle.rb
+++ app/models/feature_toggle.rb
@@ -2,6 +2,6 @@ class FeatureToggle < ApplicationRecord
   belongs_to :user
 
   validates :user, presence: true
-  validates :name, length: { minimum: 5 }
+  validates :name, length: { in: 5..50 }
   validates :name, presence: true
 end
--- spec/models/feature_toggle_spec.rb
+++ spec/models/feature_toggle_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe FeatureToggle, type: :model do
 
   context "validations" do
     it { is_expected.to validate_presence_of(:user) }
-    it { is_expected.to validate_length_of(:name).is_at_least(5) }
+    it { is_expected.to validate_length_of(:name).is_at_least(5).is_at_most(50) }
     it { is_expected.to validate_presence_of(:name) }
   end
 end
