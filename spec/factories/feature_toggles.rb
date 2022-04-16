e7ddbeece513b784dca621cf29b9f9047e15038a
Rommy Gustiawan <rgcppp@gmail.com>
2022-04-16 16:42:43 +0700

    refactor: use admin trait for user on feature toggle factory
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/factories/feature_toggles.rb
+++ spec/factories/feature_toggles.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :feature_toggle do
-    user
+    association :user, :admin
     sequence(:name) { |n| "ALLOW_USERS_TO_DELETE_ACCOUNT#{n}" }
 
     trait :unformatted do
