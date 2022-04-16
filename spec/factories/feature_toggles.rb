4da0fe623baab65422b81f91aed1c69aa95a3d1b
Rommy Gustiawan <rgcppp@gmail.com>
2022-04-16 10:04:14 +0700

    chore: add unformatted trait on feature toggle factory
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/factories/feature_toggles.rb
+++ spec/factories/feature_toggles.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :feature_toggle do
     user
     sequence(:name) { |n| "ALLOW_USERS_TO_DELETE_ACCOUNT#{n}" }
+
+    trait :unformatted do
+      name { " ALLOW_USERS_TO_DELETE_ACCOUNT " }
+    end
   end
 end
