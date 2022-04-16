85bc5043e3f7df6d33b10396dadd195221f295f9
Rommy Gustiawan <rgcppp@gmail.com>
2022-04-16 16:35:32 +0700

    chore: add admin trait on user factory
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,10 @@ FactoryBot.define do
     sequence(:username) { |n| "john.doe#{n}" }
     password { "12345678" }
 
+    trait :admin do
+      role { :admin }
+    end
+
     trait :invalid do
       username { "" }
       password { "" }
