54f53117e3f6103020f8745af2f927a728c90a6c
Rommy Gustiawan <rgcppp@gmail.com>
2022-04-24 09:59:51 +0700

    feat: GET /users/:id/visitations returns not found response when visitations not exist
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/controllers/api/v1/user/visitations_controller.rb
+++ app/controllers/api/v1/user/visitations_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::User::VisitationsController < ApplicationController
   before_action :authenticate_request, only: :index
 
   def index
-    forbidden_response unless @auth_id == params[:user_id].to_i
+    return forbidden_response unless @auth_id == params[:user_id].to_i
+
+    visitations = @auth_user.visitations
+
+    not_found_response unless visitations.present?
   end
 end
