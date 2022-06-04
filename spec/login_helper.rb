edefbd0bc2afbb8b016aec8414c20c3b0b04ce09
Rommy Gustiawan <rgcppp@gmail.com>
2022-06-04 20:38:50 +0700

    chore: add session_id in encoded JWT token
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/login_helper.rb
+++ spec/login_helper.rb
@@ -1,5 +1,6 @@
 module LoginHelper
   def login(user_id)
-    JsonWebToken.encode({ user_id: user_id })
+    session_id = create(:session, user_id: user_id).id
+    JsonWebToken.encode({ user_id: user_id, session_id: session_id })
   end
 end
