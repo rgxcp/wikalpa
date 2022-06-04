34de2a8399075a593e04a1f1cd9bfb44739958e1
Rommy Gustiawan <rgcppp@gmail.com>
2022-06-04 19:06:29 +0700

    chore: add script to run migrations on dev & test env
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

---
+++ bin/run_migrations.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+GREEN="\e[32m"
+WHITE="\e[0m"
+
+main () {
+    echo -e "${GREEN}[i] Running migrations on development env.${WHITE}"
+    bin/rails db:migrate
+    echo -e "${GREEN}[i] Running migrations on test env.${WHITE}"
+    RAILS_ENV=test bin/rails db:migrate
+}
+
+main
