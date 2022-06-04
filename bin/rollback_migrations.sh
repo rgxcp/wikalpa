4d50417b22a5a3c930e17cb7ed0c9f57b62e569d
Rommy Gustiawan <rgcppp@gmail.com>
2022-06-04 19:06:44 +0700

    chore: add script to rollback migrations on dev & test env
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

---
+++ bin/rollback_migrations.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+GREEN="\e[32m"
+WHITE="\e[0m"
+
+main () {
+    echo -e "${GREEN}[i] Rollback migrations on development env.${WHITE}"
+    bin/rails db:rollback
+    echo -e "${GREEN}[i] Rollback migrations on test env.${WHITE}"
+    RAILS_ENV=test bin/rails db:rollback
+}
+
+main
