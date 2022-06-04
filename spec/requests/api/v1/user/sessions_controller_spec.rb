2325d2829281724b080c99575092db7e3ae1568b
Rommy Gustiawan <rgcppp@gmail.com>
2022-06-04 12:16:55 +0700

    feat: GET /users/:id/sessions returns forbidden response when viewing someone else sessions
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/requests/api/v1/user/sessions_controller_spec.rb
+++ spec/requests/api/v1/user/sessions_controller_spec.rb
@@ -19,5 +19,29 @@ RSpec.describe "Api::V1::User::SessionsController", type: :request do
         expect(result["message"]).to eq("Unauthorized")
       end
     end
+
+    context "when viewing someone else sessions" do
+      before do
+        user1 = create(:user)
+        user2 = create(:user)
+        token = JsonWebToken.encode({ id: user2.id })
+        headers = { Authorization: "Bearer #{token}" }
+        get api_v1_user_sessions_path(user1), headers: headers
+      end
+
+      it "returns 403 status code" do
+        expect(response).to have_http_status(:forbidden)
+      end
+
+      it "returns false success body" do
+        result = JSON.parse(response.body)
+        expect(result["success"]).to be(false)
+      end
+
+      it "returns forbidden message body" do
+        result = JSON.parse(response.body)
+        expect(result["message"]).to eq("Forbidden")
+      end
+    end
   end
 end
