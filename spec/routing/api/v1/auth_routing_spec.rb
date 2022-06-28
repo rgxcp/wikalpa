c7d439b95cef10e2026dee655fe0c0d30ac31442
Rommy Gustiawan <rgcppp@gmail.com>
2022-06-28 19:42:20 +0700

    test: add AuthController routing spec
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

---
+++ spec/routing/api/v1/auth_routing_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::AuthController, type: :routing do
+  it { is_expected.to route(:post, "/api/v1/auth/login").to(controller: "api/v1/auth", action: :login) }
+  it { is_expected.to route(:post, "/api/v1/auth/register").to(controller: "api/v1/auth", action: :register) }
+  it { is_expected.to route(:delete, "/api/v1/auth/logout").to(controller: "api/v1/auth", action: :logout) }
+end
