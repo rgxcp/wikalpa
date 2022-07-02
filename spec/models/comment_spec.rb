844d495ea536d2e1c05adea27cc365221e5bd899
Rommy Gustiawan <rgcppp@gmail.com>
2022-07-02 22:18:34 +0700

    test: add Comment db columns spec
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,17 @@
 require "rails_helper"
 
 RSpec.describe Comment, type: :model do
+  describe "db columns" do
+    it { is_expected.to have_db_column(:post_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:user_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:body).of_type(:string).with_options(limit: 255, null: false) }
+    it { is_expected.to have_db_column(:is_spoiler).of_type(:boolean).with_options(default: false, null: false) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(precision: 6, null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(precision: 6, null: false) }
+    it { is_expected.to have_db_index(:post_id) }
+    it { is_expected.to have_db_index(:user_id) }
+  end
+
   describe "relations" do
     it { is_expected.to belong_to(:post) }
     it { is_expected.to belong_to(:user) }
