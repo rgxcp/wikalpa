608bd22c2c13b6e82391a6c22f35ca125d4d326f
Rommy Gustiawan <rgcppp@gmail.com>
2022-07-02 22:04:36 +0700

    test: add CollectionItem db columns spec
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/models/collection_item_spec.rb
+++ spec/models/collection_item_spec.rb
@@ -1,6 +1,16 @@
 require "rails_helper"
 
 RSpec.describe CollectionItem, type: :model do
+  describe "db columns" do
+    it { is_expected.to have_db_column(:collection_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:collectable_type).of_type(:string).with_options(null: false) }
+    it { is_expected.to have_db_column(:collectable_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(precision: 6, null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(precision: 6, null: false) }
+    it { is_expected.to have_db_index([:collectable_type, :collectable_id]) }
+    it { is_expected.to have_db_index(:collection_id) }
+  end
+
   describe "relations" do
     it { is_expected.to belong_to(:collectable) }
     it { is_expected.to belong_to(:collection) }
