c50934c6230f09b248a4a81ec75d01367f25c27d
Rommy Gustiawan <rgcppp@gmail.com>
2022-07-02 21:56:05 +0700

    test: add Buddy db columns spec
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/models/buddy_spec.rb
+++ spec/models/buddy_spec.rb
@@ -1,6 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Buddy, type: :model do
+  describe "db columns" do
+    it { is_expected.to have_db_column(:user_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:buddy_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(precision: 6, null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(precision: 6, null: false) }
+    it { is_expected.to have_db_index(:buddy_id) }
+    it { is_expected.to have_db_index(:user_id) }
+  end
+
   describe "relations" do
     it { is_expected.to belong_to(:buddy).class_name("User") }
     it { is_expected.to belong_to(:user) }
