8928ce5e69a7f1c8d80e6db15a252609a0a7b235
Rommy Gustiawan <rgcppp@gmail.com>
2022-07-03 08:49:03 +0700

    test: add Upvote db columns spec
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/models/upvote_spec.rb
+++ spec/models/upvote_spec.rb
@@ -1,6 +1,16 @@
 require "rails_helper"
 
 RSpec.describe Upvote, type: :model do
+  describe "db columns" do
+    it { is_expected.to have_db_column(:user_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:upvoteable_type).of_type(:string).with_options(null: false) }
+    it { is_expected.to have_db_column(:upvoteable_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(precision: 6, null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(precision: 6, null: false) }
+    it { is_expected.to have_db_index([:upvoteable_type, :upvoteable_id]) }
+    it { is_expected.to have_db_index(:user_id) }
+  end
+
   describe "relations" do
     it { is_expected.to belong_to(:upvoteable) }
     it { is_expected.to belong_to(:user) }
