5cb2940a5fb01810dcdef031cdf3870135be2391
Rommy Gustiawan <rgcppp@gmail.com>
2022-07-03 08:47:20 +0700

    test: add Session db columns spec
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,6 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Session, type: :model do
+  describe "db columns" do
+    it { is_expected.to have_db_column(:user_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:device).of_type(:string).with_options(limit: 255, null: false) }
+    it { is_expected.to have_db_column(:ip).of_type(:inet).with_options(null: false) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(precision: 6, null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(precision: 6, null: false) }
+    it { is_expected.to have_db_index(:user_id) }
+  end
+
   describe "relations" do
     it { is_expected.to belong_to(:user) }
   end
