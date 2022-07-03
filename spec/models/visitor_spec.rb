935899a26bc16d0759a6dd56bd7a93f4e190d907
Rommy Gustiawan <rgcppp@gmail.com>
2022-07-03 08:57:29 +0700

    test: add Visitor db columns spec
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/models/visitor_spec.rb
+++ spec/models/visitor_spec.rb
@@ -1,6 +1,16 @@
 require "rails_helper"
 
 RSpec.describe Visitor, type: :model do
+  describe "db columns" do
+    it { is_expected.to have_db_column(:user_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:visitable_type).of_type(:string).with_options(null: false) }
+    it { is_expected.to have_db_column(:visitable_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(precision: 6, null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(precision: 6, null: false) }
+    it { is_expected.to have_db_index([:visitable_type, :visitable_id]) }
+    it { is_expected.to have_db_index(:user_id) }
+  end
+
   describe "relations" do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:visitable) }
