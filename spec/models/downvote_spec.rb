ab6c010b3fa6df03e3dda8996400734e7524de81
Rommy Gustiawan <rgcppp@gmail.com>
2022-07-03 08:34:51 +0700

    test: add Downvote db columns spec
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/models/downvote_spec.rb
+++ spec/models/downvote_spec.rb
@@ -1,6 +1,16 @@
 require "rails_helper"
 
 RSpec.describe Downvote, type: :model do
+  describe "db columns" do
+    it { is_expected.to have_db_column(:user_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:downvoteable_type).of_type(:string).with_options(null: false) }
+    it { is_expected.to have_db_column(:downvoteable_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(precision: 6, null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(precision: 6, null: false) }
+    it { is_expected.to have_db_index([:downvoteable_type, :downvoteable_id]) }
+    it { is_expected.to have_db_index(:user_id) }
+  end
+
   describe "relations" do
     it { is_expected.to belong_to(:downvoteable) }
     it { is_expected.to belong_to(:user) }
