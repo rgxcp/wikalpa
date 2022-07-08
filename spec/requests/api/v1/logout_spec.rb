e967dab0abd4caa27ee2025e5e35959693628370
Rommy Gustiawan <rgcppp@gmail.com>
2022-07-08 21:38:51 +0700

    refactor: move test /auth/logout to logout_spec
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

---
+++ spec/requests/api/v1/logout_spec.rb
@@ -0,0 +1,47 @@
+require "rails_helper"
+
+RSpec.describe "Logout from current session", type: :request do
+  let(:parsed_body) { JSON.parse(response.body) }
+
+  context "when user not logged in" do
+    before { delete api_v1_auth_logout_path }
+
+    it "returns unauthorized response" do
+      expect(response).to have_http_status(:unauthorized)
+      expect(parsed_body["success"]).to be(false)
+      expect(parsed_body["message"]).to eq("Unauthorized")
+    end
+  end
+
+  context "when session invalid" do
+    before do
+      user = create(:user)
+      token = login(user.id)
+      user.sessions.first.destroy
+      headers = { Authorization: "Bearer #{token}" }
+      delete api_v1_auth_logout_path, headers: headers
+    end
+
+    it "returns unauthorized response" do
+      expect(response).to have_http_status(:unauthorized)
+      expect(parsed_body["success"]).to be(false)
+      expect(parsed_body["message"]).to eq("Unauthorized")
+    end
+  end
+
+  context "when session valid" do
+    before do
+      user = create(:user)
+      token = login(user.id)
+      headers = { Authorization: "Bearer #{token}" }
+      delete api_v1_auth_logout_path, headers: headers
+    end
+
+    it "returns ok response" do
+      expect(response).to have_http_status(:ok)
+      expect(parsed_body["success"]).to be(true)
+      expect(parsed_body["message"]).to eq("OK")
+      expect(parsed_body["data"]["session"]).to be_present
+    end
+  end
+end
