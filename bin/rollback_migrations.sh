a895fe8f419dc1240977187d172c9c59c008b037
Rommy Gustiawan <rgcppp@gmail.com>
2022-07-09 20:49:54 +0700

    style: format document
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- bin/rollback_migrations.sh
+++ bin/rollback_migrations.sh
@@ -4,10 +4,10 @@ GREEN="\e[32m"
 WHITE="\e[0m"
 
 main () {
-    echo -e "${GREEN}[i] Rollback migrations on development env.${WHITE}"
-    bin/rails db:rollback
-    echo -e "${GREEN}[i] Rollback migrations on test env.${WHITE}"
-    RAILS_ENV=test bin/rails db:rollback
+	echo -e "${GREEN}[i] Rollback migrations on development env.${WHITE}"
+	bin/rails db:rollback
+	echo -e "${GREEN}[i] Rollback migrations on test env.${WHITE}"
+	RAILS_ENV=test bin/rails db:rollback
 }
 
 main
--- bin/run_migrations.sh
+++ bin/run_migrations.sh
@@ -4,10 +4,10 @@ GREEN="\e[32m"
 WHITE="\e[0m"
 
 main () {
-    echo -e "${GREEN}[i] Running migrations on development env.${WHITE}"
-    bin/rails db:migrate
-    echo -e "${GREEN}[i] Running migrations on test env.${WHITE}"
-    RAILS_ENV=test bin/rails db:migrate
+	echo -e "${GREEN}[i] Running migrations on development env.${WHITE}"
+	bin/rails db:migrate
+	echo -e "${GREEN}[i] Running migrations on test env.${WHITE}"
+	RAILS_ENV=test bin/rails db:migrate
 }
 
 main
