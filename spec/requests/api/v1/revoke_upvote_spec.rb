c5fba5ecb31ec562519a7bf40679e2ded2966ebe
Rommy Gustiawan <rgcppp@gmail.com>
2022-08-11 18:51:42 +0700

    refactor: [#182923079] move `DELETE /upvotes/:id` request test to revoke_upvote_spec
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

---
+++ spec/requests/api/v1/revoke_upvote_spec.rb
@@ -0,0 +1,68 @@
+require "rails_helper"
+
+RSpec.describe "Revoke upvote an entity", type: :request do
+  let(:parsed_body) { JSON.parse(response.body) }
+
+  context "when user not logged in" do
+    before { delete api_v1_upvote_path(1) }
+
+    it "returns unauthorized response" do
+      expect(response).to have_http_status(:unauthorized)
+      expect(parsed_body["success"]).to be(false)
+      expect(parsed_body["message"]).to eq("Unauthorized")
+    end
+  end
+
+  context "when upvote not exists" do
+    before do
+      user = create(:user)
+      token = login(user.id)
+      headers = { Authorization: "Bearer #{token}" }
+      delete api_v1_upvote_path(0), headers: headers
+    end
+
+    it "returns not found response" do
+      expect(response).to have_http_status(:not_found)
+      expect(parsed_body["success"]).to be(false)
+      expect(parsed_body["message"]).to eq("Not Found")
+    end
+  end
+
+  context "when deleting someone else upvote" do
+    before do
+      community = create(:community)
+      user1 = create(:user)
+      user2 = create(:user)
+      post = create(:post, community: community, user: user1)
+      upvote = create(:upvote, user: user2, upvoteable: post)
+      token = login(user1.id)
+      headers = { Authorization: "Bearer #{token}" }
+      delete api_v1_upvote_path(upvote), headers: headers
+    end
+
+    it "returns forbidden response" do
+      expect(response).to have_http_status(:forbidden)
+      expect(parsed_body["success"]).to be(false)
+      expect(parsed_body["message"]).to eq("Forbidden")
+    end
+  end
+
+  context "when upvote exists" do
+    before do
+      community = create(:community)
+      user = create(:user)
+      post = create(:post, community: community, user: user)
+      upvote = create(:upvote, user: user, upvoteable: post)
+      token = login(user.id)
+      headers = { Authorization: "Bearer #{token}" }
+      delete api_v1_upvote_path(upvote), headers: headers
+    end
+
+    it "returns ok response" do
+      expect(response).to have_http_status(:ok)
+      expect(parsed_body["success"]).to be(true)
+      expect(parsed_body["message"]).to eq("OK")
+      expect(parsed_body["data"]["upvote"]).to be_present
+    end
+  end
+end
