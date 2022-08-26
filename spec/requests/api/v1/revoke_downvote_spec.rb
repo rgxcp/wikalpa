a9289160df049a2ad04c6c0b6b639491937af706
Rommy Gustiawan <rgcppp@gmail.com>
2022-08-26 20:14:38 +0700

    chore: [#183039745] add missing context on revoke downvote spec
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/requests/api/v1/revoke_downvote_spec.rb
+++ spec/requests/api/v1/revoke_downvote_spec.rb
@@ -46,20 +46,23 @@ RSpec.describe "Revoke downvote an entity", type: :request do
       expect(parsed_body["message"]).to eq("Forbidden")
     end
   end
-  before do
-    community = create(:community)
-    user = create(:user)
-    post = create(:post, community: community, user: user)
-    downvote = create(:downvote, user: user, downvoteable: post)
-    token = login(user.id)
-    headers = { Authorization: "Bearer #{token}" }
-    delete api_v1_downvote_path(downvote), headers: headers
-  end
 
-  it "returns ok response" do
-    expect(response).to have_http_status(:ok)
-    expect(parsed_body["success"]).to be(true)
-    expect(parsed_body["message"]).to eq("OK")
-    expect(parsed_body["data"]["downvote"]).to be_present
+  context "when downvote exists" do
+    before do
+      community = create(:community)
+      user = create(:user)
+      post = create(:post, community: community, user: user)
+      downvote = create(:downvote, user: user, downvoteable: post)
+      token = login(user.id)
+      headers = { Authorization: "Bearer #{token}" }
+      delete api_v1_downvote_path(downvote), headers: headers
+    end
+
+    it "returns ok response" do
+      expect(response).to have_http_status(:ok)
+      expect(parsed_body["success"]).to be(true)
+      expect(parsed_body["message"]).to eq("OK")
+      expect(parsed_body["data"]["downvote"]).to be_present
+    end
   end
 end
