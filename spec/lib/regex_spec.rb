55130b24a57b2269bf89ad8c028359da5e8543b4
Rommy Gustiawan <rgcppp@gmail.com>
2022-09-01 20:57:57 +0700

    refactor: [#183154802] change 'it' to 'context' on REGEX spec
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- spec/lib/regex_spec.rb
+++ spec/lib/regex_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 require "regex"
 
 RSpec.describe REGEX do
-  it "::FEATURE_TOGGLE_NAME" do
-    expect(REGEX::FEATURE_TOGGLE_NAME).to eq(/\A\S+\z/)
+  context "::FEATURE_TOGGLE_NAME" do
+    it { expect(REGEX::FEATURE_TOGGLE_NAME).to eq(/\A\S+\z/) }
   end
 
-  it "::NAME" do
-    expect(REGEX::NAME).to eq(/\A[a-zA-Z0-9.\w]+\z/)
+  context "::NAME" do
+    it { expect(REGEX::NAME).to eq(/\A[a-zA-Z0-9.\w]+\z/) }
   end
 end
